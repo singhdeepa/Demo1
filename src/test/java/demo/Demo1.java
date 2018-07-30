7cf5ab02e2dacd475108ee699374ed0c27ec4655
singhdeepa <33508096+singhdeepa@users.noreply.github.com>
2018-07-30 10:55:01 +0530

    Update Demo1.java

--- src/test/java/demo/Demo1.java
+++ src/test/java/demo/Demo1.java
@@ -11,16 +11,7 @@ public class Demo1 {
 @Test
 public void test() throws InterruptedException
 {
-	WebDriver driver=new FirefoxDriver();
-	 driver.get("https://www.google.com/gmail/about/");
-     driver.findElement(By.xpath("//a[.='Sign In']")).click();        
-     driver.findElement(By.id("identifierId")).sendKeys("singhdeepa9555");
-    // test.log(LogStatus.PASS, "username is entered");
-     driver.findElement(By.xpath("//span[.='Next']")).click();
-    // test.log(LogStatus.PASS, "Next Button is clicked");
-     Thread.sleep(3000);
-     driver.findElement(By.name("password")).sendKeys("nilam_63");
-     Thread.sleep(3000);
-     driver.findElement(By.xpath("//span[.='Next']")).click();
+	system.out.println("hello");
+			   
 }
 }
